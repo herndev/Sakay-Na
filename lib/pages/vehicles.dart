cad5eba9d328f4dd020e6f8b3b4de1ee7eda7dfa
herndev <herniejabien45@gmail.com>
2022-05-25 19:41:28 +0800

    Updated

---
+++ lib/pages/vehicles.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sakayna/components/appbar.dart';
+import 'package:sakayna/services/authentication.dart';
+
+class Locations extends StatefulWidget {
+  const Locations({Key? key}) : super(key: key);
+
+  @override
+  State<Locations> createState() => _LocationsState();
+}
+
+class _LocationsState extends State<Locations> {
+  var origins = ["cogon", "ustp"];
+  var origin = "cogon";
+  var destinations = ["cogon", "ustp"];
+  var destination = "ustp";
+
+  @override
+  Widget build(BuildContext context) {
+    AppBar appBar = AppBar(
+      title: Text('Demo'),
+    );
+    double appbarheight = appBar.preferredSize.height;
+    var statusBar = MediaQuery.of(context).viewPadding.top;
+    var size = MediaQuery.of(context).size;
+    final auth = Provider.of<AuthService>(context);
+
+    return Scaffold(
+      appBar: appbar(title: "Locations"),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+          child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Stack(
+            overflow: Overflow.visible,
+            children: [
+              Container(
+                width: size.width * 0.9,
+                height: size.height - statusBar - appbarheight,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [],
+                ),
+              ),
+            ],
+          ),
+        ],
+      )),
+    );
+  }
+}
