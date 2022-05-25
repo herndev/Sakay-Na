177f0a902ab2b8c259f330b8385f911d2a42ac66
herndev <herniejabien45@gmail.com>
2022-05-25 11:54:24 +0800

    Updated

--- lib/pages/select_vehicle.dart
+++ lib/pages/select_vehicle.dart
@@ -11,10 +11,38 @@ class SelectVehicle extends StatefulWidget {
 }
 
 class _SelectVehicleState extends State<SelectVehicle> {
+  var data = [
+    {
+      "image": "assets/tricycle.png",
+      "vehicle": "Tricycle",
+    },
+    {
+      "image": "assets/jeep.png",
+      "vehicle": "R1 Jeep",
+    },
+    {
+      "image": "assets/sikad.png",
+      "vehicle": "Padyak",
+    },
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 1), () {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Swipe to select vehicle"),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var statusBar = MediaQuery.of(context).viewPadding.top;
     var size = MediaQuery.of(context).size;
+    var _current = 0;
 
     return Scaffold(
       backgroundColor: Colors.cyan.shade700,
@@ -23,65 +51,86 @@ class _SelectVehicleState extends State<SelectVehicle> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: size.width * 0.8,
-                height: size.height - statusBar,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CarouselSlider(
-                      options: CarouselOptions(height: 400.0),
-                      items: [1, 2, 3, 4, 5].map((i) {
-                        return Builder(
-                          builder: (BuildContext context) {
-                            return Container(
-                                width: MediaQuery.of(context).size.width,
-                                margin: EdgeInsets.symmetric(horizontal: 5.0),
-                                decoration: BoxDecoration(color: Colors.amber),
-                                child: Text(
-                                  'text $i',
-                                  style: TextStyle(fontSize: 16.0),
-                                ));
-                          },
-                        );
-                      }).toList(),
-                    ),
-                    CircleAvatar(
-                      radius: 100,
-                      backgroundImage: AssetImage("assets/tricycle.png"),
-                    ),
-                    // Image.asset("assets/jeep.png"),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      "Origin",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 21,
+              Stack(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 15.0),
+                      child: Icon(
+                        Icons.arrow_back,
+                        color: Colors.amber.shade300,
                       ),
                     ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    SizedBox(height: 15),
-                    Container(
-                      width: double.infinity,
-                      child: ElevatedButton(
-                        onPressed: () async {},
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 15),
-                          child: Text(
-                            "SELECT",
-                            style: TextStyle(color: Colors.black),
+                  ),
+                  Container(
+                    width: size.width * 0.8,
+                    height: size.height - statusBar,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CarouselSlider(
+                          options: CarouselOptions(
+                            initialPage: 0,
+                            height: 271.0,
+                            enlargeCenterPage: true,
+                            enableInfiniteScroll: false,
+                            onScrolled: (index) {
+                              _current = index!.toInt();
+                              setState(() {});
+                            },
+                          ),
+                          items: data.map((i) {
+                            return Builder(
+                              builder: (BuildContext context) {
+                                return Column(
+                                  children: [
+                                    CircleAvatar(
+                                      radius: 100,
+                                      backgroundImage: AssetImage(i['image']!),
+                                    ),
+                                    // Image.asset("assets/jeep.png"),
+                                    SizedBox(
+                                      height: 8,
+                                    ),
+                                    Text(
+                                      i['vehicle']!,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 21,
+                                      ),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }).toList(),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        SizedBox(height: 15),
+                        Container(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            onPressed: () async {},
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 15),
+                              child: Text(
+                                "SELECT",
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ),
+                            style: ElevatedButton.styleFrom(primary: Colors.amber.shade300),
                           ),
                         ),
-                        style: ElevatedButton.styleFrom(primary: Colors.amber.shade300),
-                      ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ],
           ),
