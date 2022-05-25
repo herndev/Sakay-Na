c2e5c7f3f9a5fe6a66597386140a54bd90ec40c8
herndev <herniejabien45@gmail.com>
2022-05-25 10:24:55 +0800

    Updated

--- lib/pages/select_vehicle.dart
+++ lib/pages/select_vehicle.dart
@@ -30,22 +30,22 @@ class _SelectVehicleState extends State<SelectVehicle> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     CarouselSlider(
-  options: CarouselOptions(height: 400.0),
-  items: [1,2,3,4,5].map((i) {
-    return Builder(
-      builder: (BuildContext context) {
-        return Container(
-          width: MediaQuery.of(context).size.width,
-          margin: EdgeInsets.symmetric(horizontal: 5.0),
-          decoration: BoxDecoration(
-            color: Colors.amber
-          ),
-          child: Text('text $i', style: TextStyle(fontSize: 16.0),)
-        );
-      },
-    );
-  }).toList(),
-)
+                      options: CarouselOptions(height: 400.0),
+                      items: [1, 2, 3, 4, 5].map((i) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                                width: MediaQuery.of(context).size.width,
+                                margin: EdgeInsets.symmetric(horizontal: 5.0),
+                                decoration: BoxDecoration(color: Colors.amber),
+                                child: Text(
+                                  'text $i',
+                                  style: TextStyle(fontSize: 16.0),
+                                ));
+                          },
+                        );
+                      }).toList(),
+                    ),
                     CircleAvatar(
                       radius: 100,
                       backgroundImage: AssetImage("assets/tricycle.png"),
