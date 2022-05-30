9335b01eb90d888b9633726bf0edc7c706814b33
herndev <herniejabien45@gmail.com>
2022-05-30 09:22:07 +0800

    Updated

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -80,7 +80,7 @@ class _HomeState extends State<Home> {
                       selectedLocation = locations[0];
                     }
                     if (selectedLocation2 == "") {
-                      selectedLocation2 = locations[0];
+                      selectedLocation2 = locations[1];
                     }
 
                     return Container(
